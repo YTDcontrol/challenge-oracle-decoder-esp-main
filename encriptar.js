f3b2d4e0bfd2f62129295ea4e149c4dd759326ff
Adrián Sosa <adriansosa105@gmail.com>
2022-02-08 16:48:30 -0300

    Encription finished.

--- encriptar.js
+++ encriptar.js
@@ -10,9 +10,10 @@
 // }
 
 var caracterPulsado = document.querySelector("#input-texto");
+var mensajeEncriptado = document.querySelector("#msg");
 var caracterErrado = document.querySelector("#error-caracter");
-var caracteresAdmitidos = "qwertyuiopasd fghjklñzxcvbnm,;.:!";
 var botonEncriptar = document.querySelector("#btn-encriptar");
+var caracteresAdmitidos = "qwertyuiopasd fghjklñzxcvbnm,;.:!";
 
 caracterPulsado.addEventListener("input", function(){
     var entrada = this.value;
@@ -27,8 +28,34 @@ caracterPulsado.addEventListener("input", function(){
     }
 });
 
-botonEncriptar.addEventListener("click", encriptar);
+botonEncriptar.addEventListener("click", function(event){
+    event.preventDefault();
 
-function encriptar(){
-    
-}
+    var textoEncriptado = [];
+    var textoIngresado = caracterPulsado.value;
+
+    for (var x=0; x<textoIngresado.length; x++){
+        switch (textoIngresado[x]) {
+          case "a":
+            textoEncriptado += "ai";
+            break;
+          case "e":
+            textoEncriptado += "enter";
+            break;
+          case "i":
+            textoEncriptado += "imes";
+            break;
+          case "o":
+            textoEncriptado += "ober";
+            break;
+          case "u":
+            textoEncriptado += "ufat";
+            break;
+
+          default:
+            textoEncriptado += textoIngresado[x];
+            break;
+        }
+    }
+    mensajeEncriptado.value = textoEncriptado;
+});
