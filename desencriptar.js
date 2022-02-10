5938d65beeeb9c9c07ca4996069506c769645448
Adrián Sosa <adriansosa105@gmail.com>
2022-02-10 11:12:34 -0300

    Desencription for character 'a' ready.

--- desencriptar.js
+++ desencriptar.js
@@ -9,9 +9,26 @@ botonCopiar.addEventListener("click", function (event) {
 
     const elem = document.createElement("textarea");
     elem.value = mensajeEncriptado.value;
-    console.log(elem.value);
     document.body.appendChild(elem);
     elem.select();
     document.execCommand("copy");
     document.body.removeChild(elem); 
+});
+
+botonDesencriptar.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    var textoDesEncriptado = [];
+    var textoIngresado = caracterPulsado.value;
+
+    //console.log(textoIngresado);
+    var posicion = textoIngresado.indexOf("ai");
+    while (posicion >= 0) {
+        textoIngresado =
+          textoIngresado.slice(0, posicion) +
+          "a" +
+          textoIngresado.slice(posicion + 2);
+        posicion = textoIngresado.indexOf("ai");
+    }
+    console.log(textoIngresado);
 });
